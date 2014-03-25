c2811fbdc4c384b9523e72b52090e7e6df8390d6
Krzysztof Raciniewski <raciniak@mat.umk.pl>
2014-03-25 22:31:30 +0100

    Utworzyłem podstawową strukturę plików i dodałem wszystko, nad czym pracował Jacek

---
+++ js/player.js
@@ -0,0 +1,138 @@
+var intID;
+var a='';
+var i;
+var vid, plbtn, slider, czasObecny, czasTrwania;
+$(document).ready(function(){
+       pobierzPlik();
+       $('#load_JPG').html('<img src="logo.png" width="640" height="360" alt="cos1"/>');
+       document.getElementById("film").src=getImagePath();
+       startPlayer();
+       $('#playPause').click(function(){i=1;});
+       //$('#playPause').mouseover(function(){alert("los")}); 
+});
+
+function obrazek(){
+        var czas = Math.floor(vid.currentTime);
+        var j=5;
+        var i=0;
+        while(i==0)
+        {
+            if(czas>=parseInt(a[j]))
+                i=j;
+            if(j==0)
+                i=a[0];
+            j--;
+
+         }
+
+        if(i<parseInt(a[0])+1)
+        {
+             if(czas>=a[i])
+             {
+                 $('#load_JPG').html('<img src="../res/test2/' + a[i] + '.jpg" width="640" height="360" alt="cos'+i+'"/>');
+             }
+        }
+}
+
+
+function getImagePath(){
+     return "../res/" + getParameterByName("id") + "/speech.mp4";
+   }
+
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function startPlayer() {
+    vid = document.getElementById("vid");
+    vid.load();
+    plbtn = document.getElementById("playPause");
+    slider = document.getElementById("slider");
+    czasObecny = document.getElementById("czasobecny");
+    czasTrwania = document.getElementById("czastrwania");
+    mute = document.getElementById("mute");
+    volslider = document.getElementById("volslider");
+    //obsluga elementow
+    plbtn.addEventListener("click", playPause, false);
+    slider.addEventListener("change", vidSeek, false);
+    vid.addEventListener("timeupdate", seekTimeUpdate, false);
+    vid.addEventListener("timeupdate", obrazek, false);
+    mute.addEventListener("click", vidmute, false);
+    volslider.addEventListener("change", volume, false);
+}
+
+
+function pobierzPlik()
+            {
+                
+		var txt='';
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("GET","../res/test2/times.txt");
+		xmlhttp.onreadystatechange = function(){
+			if(xmlhttp.status==200 && xmlhttp.readyState==4){
+				txt=xmlhttp.responseText;
+                                a=txt.split('\n');
+			}
+                            
+		};
+		xmlhttp.send();
+            }
+
+function playPause() {
+    if (vid.paused) {
+        vid.play();
+        plbtn.textContent = "PAUZA";
+    } else {
+        vid.pause();
+        plbtn.textContent = "GRAJ";
+    }
+}
+
+function vidSeek() {
+    var seekto = vid.duration * (slider.value / 100);
+    vid.currentTime = seekto;
+}
+
+function seekTimeUpdate() {
+    var nt = vid.currentTime * (100 / vid.duration);
+    slider.value = nt;
+    var min = Math.floor(vid.currentTime / 60);
+    var sek = Math.floor(vid.currentTime - min * 60);
+    var dmin = Math.floor(vid.duration / 60);
+    var dsek = Math.round(vid.duration - dmin * 60);
+    if (sek < 10) {
+        sek = "0" + sek;
+    }
+    if (dsek < 10) {
+        dsek = "0" + dsek;
+    }
+    if (min < 10) {
+        min = "0" + min;
+    }
+    if (dmin < 10) {
+        dmin = "0" + dmin;
+    }
+    czasObecny.innerHTML = min + ":" + sek;
+    czasTrwania.innerHTML = dmin + ":" + dsek;
+    
+
+}
+
+function vidmute() {
+    if (vid.muted) {
+        vid.muted = false;
+        mute.textContent = "CISZA";
+    } else {
+        vid.muted = true;
+        mute.textContent = "GÅ?OÅšNO";
+    }
+}
+
+function volume() {
+    vid.volume = volslider.value / 100;
+}
+
---
+++ js/timeline.js
@@ -0,0 +1,4 @@
+
+$(document).ready(function(){
+    $('.slider').mouseover(function(){alert("los");});
+});
