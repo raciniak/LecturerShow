9f6c1f2d991835d20160e449aeb4c69fc8bde0b5
Jacek Murawski <kecaj-goku@wp.pl>
2014-03-29 19:52:14 +0100

    Kolejne upiększenie, dodanie tabow w edytorze

--- js/editor.js
+++ js/editor.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 	   //funkcja do pobierania czasow odtwarzania slajdow, kazda funkcja ktora chce korzystac z tablicy a zawierajacej
 	   // czasy musi odczekac w jakis sposob chwile aby funkcja zdazyla sie wykonac
        pobierzPlik();
+       taby();
      //  $('#load_JPG').html('<img src="images/logo.png" width="640" height="360" alt="cos1"/>');
        //zaladowanie playera z odtwarzaczem slajdow
      //  startPlayer();
@@ -20,6 +21,40 @@ $(document).ready(function(){
        var evt = new Event();
        var dragdrop = new Dragdrop(evt);
 });
+
+function taby(){
+		//dla każdego miejsca z tabami
+        $('.tabs').each(function() {
+            var $ul = $(this);
+            var $li = $ul.children('li');
+            //przy wejsciu na strone ukrywamy tresc tabow i pokazujemy tylko aktywny...
+            $li.each(function() { //pętla po wszystkich tabach
+                var $trescTaba = $($(this).children('a').attr('href')); //pobieramy blok o id pobranym z linka-taba
+                if ($(this).hasClass('active')) { //jeżeli ten tab ma klasę aktywną
+                    $trescTaba.show(); //to pobrany przed chwilą blok pokazujemy
+                } else {
+                    $trescTaba.hide(); //jeżeli takiej klasy nie ma to blok ukrywamy
+                }
+            });
+                     
+            //mały trik - gdy klikamy na tab, wtedy wykonujemy zdarzenie dla linka, który się w nim znajduje (dzieki temu możemy kliknąć na cały tab, a nie tylko na linka)
+            $li.click(function() {$(this).children('a').click();});
+            //po kliknięciu na link...
+            $li.children('a').click(function() {
+                //usuwamy z tabów klasę active
+                $li.removeClass('active');
+                //ukrywamy wszystkie taby              
+                $li.each(function() {
+                    $($(this).children('a').attr('href')).hide();
+                });
+                //ustawiamy klikniętemu tabowi klasę aktywną
+                $(this).parent().addClass('active');
+                $($(this).attr('href')).show();
+                //nie chcemy wykonać domyślnej akcji dla linka
+                return false;
+            });
+        });
+}
 
 function obrazek(){
         var czas = Math.floor(vid.currentTime);
