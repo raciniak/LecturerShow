7a75d43b0e2bcf4d7300c25ef101d59f86f90fc2
Dawid Baszewski <baszewski@mat.umk.pl>
2014-03-31 19:45:11 +0200

    Poprawa statystyk uzytkownika

--- js/xmlhttprequest.js
+++ js/xmlhttprequest.js
@@ -1,4 +1,4 @@
-
+// funkcja wysyłająca dane z formularza rejestracji 
 function sendUserData() {
 	var fd = new FormData();
 	fd.append("login", document.getElementById('login').value);
@@ -13,6 +13,7 @@ function sendUserData() {
 	xhr.send(fd);
 	}
 
+// funkcja wysyłająca dane z formularza aktualizacji danych użytkownika 
 function updateUserData() {
 	var fd = new FormData();
 	fd.append("login", document.getElementById('login_aktualizacja').value);
@@ -28,6 +29,7 @@ function updateUserData() {
 	xhr.send(fd);
 }
 
+// funkcja wykonująca się po poprawnym wykonaniu funkcji wysylajacej (sendUserData) i aktualizującej dane (updateUserData)
 function uploadComplete(evt) {
 	alert(evt.target.responseText);
 	setTimeout(function(){
@@ -35,8 +37,9 @@ function uploadComplete(evt) {
 	},100);
 }
 
+// funkcja logowania
 function logIn() {
-	//alert("loguje");
+	alert("loguje");
 	var fd = new FormData();
 	fd.append("login_l", document.getElementById('login_l').value);
 	fd.append("haslo_l", document.getElementById('haslo_l').value);
@@ -47,22 +50,15 @@ function logIn() {
 	alert("loguje");
 }
 
-function logInLoadComplete(evt) {
+// funkcja wykonująca się po poprawnym zalogowaniu się
+function logInLoadComplete() {
 	alert("zalogował");
-	var dane = JSON.parse(evt.target.responseText);
 	setTimeout(function(){
 		window.location.href="konto_new.html";
 	},5);
 }
 
-
-function logOutLoadComplete(evt) {
-	alert(evt.target.responseText);
-	setTimeout(function(){
-		window.location.href="konto_new.html";
-	},5);
-}
-
+// funkcja wylogowująca użytkownika
 function logOut() {
 	
 	var xhr = new XMLHttpRequest();
@@ -72,6 +68,24 @@ function logOut() {
 	
 }
 
+// funkcja wykonująca się po poprawnym wylogowaniu się
+function logOutLoadComplete(evt) {
+	alert(evt.target.responseText);
+	setTimeout(function(){
+		window.location.href="logowanie_new.html";
+	},5);
+}
+
+
+function funkcja()
+        	{
+        		setTimeout(function(){
+        			document.getElementById("zdjecie").src = 'images/banner.jpg';
+        		},500);
+        	}
+function funkcja_powrot(){
+	document.getElementById("zdjecie").scr = 'images/60.jpg';
+}        	
 
 $(document).ready(function(){
 	$.ajax({
@@ -79,7 +93,7 @@ $(document).ready(function(){
         success : function(msg){
 			if(msg == 'nieznany')
             {
-            	//alert(msg);
+            	
             	document.getElementById("logowanie").innerHTML = "Zaloguj";
             	document.getElementById("logowanie").href = "logowanie_new.html";
             	document.getElementById("konto").innerHTML= msg;
@@ -89,6 +103,7 @@ $(document).ready(function(){
 			}
             else
 			{	
+				
 				var dane = JSON.parse(msg);	
 				document.getElementById("login_aktualizacja").value = dane.login;	
 				document.getElementById("konto").innerHTML= dane.login;
@@ -109,14 +124,43 @@ $(document).ready(function(){
         	console.log(err);
     	}
 	});
+	// funkcja pobierająca statystyki profilu zalogowanego użytkownika
 	$.ajax({
         url: "php/statystyki_profilu.php",
         success : function(msg){
-        
+       
 			var dane = JSON.parse(msg);
 			document.getElementById("ilosc_filmow").innerHTML = "Ilość filmow: " + dane.ilosc_filmow;
 			document.getElementById("ilosc_wyswietlen").innerHTML = "Łączna ilość wyświetleń Twoich filmów: " + dane.ilosc_wyswietlen;
+			document.getElementById("srednia_ocen").innerHTML = "Średnia ocen Twoich filmów: " + dane.srednia_ocen;
 			
+			
+		},
+		error: function(err) 
+		{
+        	console.log(err);
+    	}
+	}); 
+	$.ajax({
+        url: "php/search_my_videos.php",
+        success : function(msg){
+        	alert(msg);
+        	var dane = JSON.parse(msg);
+        	
+        	
+        	// do poprawy divy i odczyt kilku filmow
+        	
+        	document.getElementById("filmy").innerHTML += "<a onmouseover = funkcja() onmouseout = funkcja_powrot() href='" + dane.sciezka + "'>" + "<img id='zdjecie'  src='images/60.jpg'></a>";
+        	document.getElementById("filmy").innerHTML += "<p> Tytul: " + dane.tytul + "</p>";
+        	document.getElementById("filmy").innerHTML += "<p> Opis: " + dane.opis + "</p>";
+        	document.getElementById("filmy").innerHTML += "<p> Autor: " + dane.autor + "</p>";
+        	document.getElementById("filmy").innerHTML += "<p> Ocena: " + dane.ocena + "</p>";
+        	document.getElementById("filmy").innerHTML += "<a href='" + dane.sciezka + "'>" + dane.tytul + "</a>";
+	
+        	
+			//alert(dane.tytul);
+			//alert(dane.opis);
+		
 			
 		},
 		error: function(err) 
